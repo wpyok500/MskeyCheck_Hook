ca7ab9a75c6ad4de3ca60b5aa43ad7dad88c31f2
wangpeiyang <wpyok500@gmail.com>
2021-11-25 20:46:09 +0800

    添加项目文件。

---
+++ 密钥检测关键字符串Hook/HookAPI.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace 密钥检测关键字符串Hook
+{
+    public unsafe class HookAPI
+    {
+        const string KERNEL32 = "kernel32.dll";
+
+        [DllImport(KERNEL32)]
+        static extern bool VirtualProtect(IntPtr lpAddress, UIntPtr dwSize, VirtualProtectionType flNewProtect, out VirtualProtectionType lpflOldProtect);
+
+        private enum VirtualProtectionType : uint
+        {
+            Execute = 0x10,
+            ExecuteRead = 0x20,
+            ExecuteReadWrite = 0x40,
+            ExecuteWriteCopy = 0x80,
+            NoAccess = 0x01,
+            Readonly = 0x02,
+            ReadWrite = 0x04,
+            WriteCopy = 0x08,
+            GuardModifierflag = 0x100,
+            NoCacheModifierflag = 0x200,
+            WriteCombineModifierflag = 0x400
+        }
+
+        private static byte[] m_OriginalBytes;
+
+        public static IntPtr TargetAddress { get; set; }
+        public static IntPtr HookAddress { get; set; }
+
+        public HookAPI(IntPtr target, IntPtr hook)
+        {
+            if (Environment.Is64BitProcess)
+                throw new NotSupportedException("X64 not supported, TODO");
+
+            TargetAddress = target;
+            HookAddress = hook;
+
+            m_OriginalBytes = new byte[5];
+            ProtectionSafeMemoryCopy(m_OriginalBytes, target);
+        }
+
+
+
+        public static void Install()
+        {
+            byte[] jmp = CreateJMP(TargetAddress, HookAddress);
+            ProtectionSafeMemoryCopy(TargetAddress, jmp);
+        }
+
+        public static void Unistall()
+        {
+            ProtectionSafeMemoryCopy(TargetAddress, m_OriginalBytes);
+        }
+
+        static void ProtectionSafeMemoryCopy(byte[] dest, IntPtr source)
+        {
+            // UIntPtr = size_t
+            var bufferSize = new UIntPtr((uint)dest.Length);
+            VirtualProtectionType oldProtection, temp;
+
+            // unprotect memory to copy buffer
+            if (!VirtualProtect(BytesToIntptr(dest), bufferSize, VirtualProtectionType.ExecuteReadWrite, out oldProtection))
+                throw new Exception("Failed to unprotect memory.");
+
+            // copy buffer to address
+            Marshal.Copy(source, dest, 0, dest.Length);
+
+            // protect back
+            if (!VirtualProtect(BytesToIntptr(dest), bufferSize, oldProtection, out temp))
+                throw new Exception("Failed to protect memory.");
+        }
+        static void ProtectionSafeMemoryCopy(IntPtr dest, byte[] source)
+        {
+            // UIntPtr = size_t
+            var bufferSize = new UIntPtr((uint)source.Length);
+            VirtualProtectionType oldProtection, temp;
+
+            // unprotect memory to copy buffer
+            if (!VirtualProtect(dest, bufferSize, VirtualProtectionType.ExecuteReadWrite, out oldProtection))
+                throw new Exception("Failed to unprotect memory.");
+
+            // copy buffer to address
+            Marshal.Copy(source, 0, dest, source.Length);
+
+            // protect back
+            if (!VirtualProtect(dest, bufferSize, oldProtection, out temp))
+                throw new Exception("Failed to protect memory.");
+        }
+
+        static IntPtr BytesToIntptr(byte[] dest)
+        {
+            IntPtr ptr = Marshal.AllocHGlobal(Marshal.SizeOf(dest.Length));
+            Marshal.Copy(dest, 0, ptr, dest.Length);
+            return ptr;
+        }
+
+        static byte[] CreateJMP(IntPtr from, IntPtr to)
+        {
+            return CreateJMP(new IntPtr(to.ToInt32() - from.ToInt32() - 5));
+        }
+
+        static byte[] CreateJMP(IntPtr relAddr)
+        {
+            var list = new List<byte>();
+            // get bytes of function address
+            var funcAddr32 = BitConverter.GetBytes(relAddr.ToInt32());
+
+            // jmp [relative addr] 
+            list.Add(0xE9); // jmp
+            list.AddRange(funcAddr32); // func addr
+
+            return list.ToArray();
+        }
+    }
+}
---
+++ 密钥检测关键字符串Hook/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 密钥检测关键字符串Hook
+{
+    class Program
+    {
+		[UnmanagedFunctionPointer(CallingConvention.StdCall, CharSet = CharSet.Unicode)]
+		private delegate int GetPID2Delegate(IntPtr FileTime, IntPtr MPID, int LangId, int dwBuildNumber, int unk, IntPtr DPID2);
+
+		[UnmanagedFunctionPointer(CallingConvention.StdCall)]
+		private delegate int WrapperGetPID2Delegate(IntPtr functionPtr, IntPtr FileTime, IntPtr MPID, int LangId, int dwBuildNumber, int unk, IntPtr DPID2);
+
+		[DllImport("kernel32.dll")]
+		internal static extern bool RtlZeroMemory(IntPtr destination, int length);
+
+		// Token: 0x06000094 RID: 148
+		[DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+		internal static extern bool SetDllDirectory(string lpPathName);
+
+		// Token: 0x06000095 RID: 149
+		[DllImport("kernel32", SetLastError = true)]
+		internal static extern IntPtr LoadLibrary(string lpFileName);
+
+		// Token: 0x06000096 RID: 150
+		[DllImport("Kernel32.dll")]
+		internal static extern IntPtr GetProcAddress(IntPtr hModule, string lpProcName);
+
+		// Token: 0x06000097 RID: 151
+		[DllImport("kernel32.dll")]
+		internal static extern bool FreeLibrary(IntPtr hModule);
+		static string string_0 = Environment.CurrentDirectory + "\\";
+		[UnmanagedFunctionPointer(CallingConvention.StdCall, CharSet = CharSet.Unicode)]
+		private delegate int fnPidGenX(string ProuctKey, string PkeyPath, string MPCID, IntPtr UnknownUsage, IntPtr PID2, IntPtr PID3, IntPtr PID4);
+		private static IntPtr hModule_base = IntPtr.Zero;
+		static void Main(string[] args)
+        {
+			
+			string ProductKeys = "VK7JG-NPHTM-C97JM-9MPGT-3V66T";
+			string pkeyconfigxml = "";
+			IntPtr intPtr = Marshal.AllocHGlobal(100);
+			RtlZeroMemory(intPtr, 50);
+			Marshal.WriteByte(intPtr, 0, 50);
+			IntPtr intPtr2 = Marshal.AllocHGlobal(164);
+			RtlZeroMemory(intPtr2, 164);
+			Marshal.WriteByte(intPtr2, 0, 164);
+			IntPtr intPtr3 = Marshal.AllocHGlobal(1272);
+			RtlZeroMemory(intPtr3, 1272);
+			Marshal.WriteByte(intPtr3, 0, 248);
+			Marshal.WriteByte(intPtr3, 1, 4);
+			IntPtr hModule = LoadLibrary("ProductKeyUtilities.dll");
+			hModule_base = hModule;
+			IntPtr procAddress = GetProcAddress(hModule, "PidGenX");
+			fnPidGenX delegateForFunctionPointer = Marshal.GetDelegateForFunctionPointer<fnPidGenX>(procAddress);
+
+			//四处HookAPI
+
+			//如果要hook该函数  
+			IntPtr HookPtr = FastCall.WrapStdCallInFastCall(Marshal.GetFunctionPointerForDelegate(new GetPID2Delegate(MyGetPID2)));
+			HookAPI HookFunc = new HookAPI(new IntPtr(hModule.ToInt32() + 50073), HookPtr);
+			HookAPI.Install();
+
+
+			pkeyconfigxml = System.Environment.CurrentDirectory + "\\pkconfig_winNext.xrm-ms";
+			int num = delegateForFunctionPointer(ProductKeys, pkeyconfigxml, "55041", (IntPtr)0, intPtr, intPtr2, intPtr3);
+			Console.WriteLine(num.ToString());
+
+			HookAPI.Unistall();
+
+			Console.ReadLine();
+		}
+		private static int MyGetPID2(IntPtr intptr_1, IntPtr intptr_2, int int_0, int int_1, int int_2, IntPtr intptr_3)
+		{
+			//两处HookAPI
+
+			HookAPI.Unistall();
+
+			int num = 0;
+			checked
+			{
+				if (hModule_base != IntPtr.Zero)
+				{
+					WrapperGetPID2Delegate wrapperGetPID2Delegate = FastCall.StdcallToFastcall<WrapperGetPID2Delegate>(FastCall.InvokePtr);
+					num = wrapperGetPID2Delegate(new IntPtr(hModule_base.ToInt32() + 50073), intptr_1, intptr_2, int_0, int_1, int_2, intptr_3);
+					Console.WriteLine("num:" + num);
+					if (num == 0)
+					{
+						//Marshal.PtrToStringUni(intptr_3);
+						object obj2 = Marshal.PtrToStructure(intptr_1, typeof(FileTime));
+						FileTime fileTime = (obj2 != null) ? ((FileTime)obj2) : default(FileTime);
+						Console.WriteLine(fileTime.ActConfigKey);
+					}
+				}
+
+				HookAPI.Install();
+				return num;
+			}
+		}
+	}
+
+	[StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto, Pack = 4)]
+	public struct FileTime
+	{
+		// Token: 0x0400006D RID: 109
+		public int index;
+
+		// Token: 0x0400006E RID: 110
+		[MarshalAs(UnmanagedType.LPWStr)]
+		public string ActConfigKey;
+	}
+}
